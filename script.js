b3dc5f7f04fcea3f22ddaf99809db447f0347356
2021gc <chavangauri2021@gmail.com>
2026-02-17 20:13:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,28 +29,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Voice input
     window.startDictation = function () {
-        if ('webkitSpeechRecognition' in window) {
-            const recognition = new webkitSpeechRecognition();
-            recognition.continuous = false;
-            recognition.interimResults = false;
-            recognition.lang = 'en-IN';
+    if ('webkitSpeechRecognition' in window) {
+        const recognition = new webkitSpeechRecognition();
+        recognition.continuous = false;
+        recognition.interimResults = false;
+        recognition.lang = 'en-IN';
 
-            recognition.start();
+        recognition.start();
 
-            recognition.onresult = function (event) {
-                const input = document.getElementById("user_input");
-                if (input) {
-                    input.value = event.results[0][0].transcript;
-                }
-                recognition.stop();
-            };
+        recognition.onresult = function (event) {
+            const text = event.results[0][0].transcript;
 
-            recognition.onerror = function () {
-                recognition.stop();
-            };
-        } else {
-            alert("Speech recognition not supported");
-        }
-    };
+            // Send to Flask backend
+            fetch("/voice", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ text: text })
+            })
+            .then(response => response.json())
+            .then(data => {
+                const chatBox = document.getElementById("chatBox");
 
-    
+                chatBox.innerHTML += `
+                    <div><b>You:</b> ${text}</div>
+                    <div><b>Bot:</b> ${data.reply}</div>
+                `;
+
+                chatBox.scrollTop = chatBox.scrollHeight;
+            });
+
+            recognition.stop();
+        };
+
+        recognition.onerror = function (event) {
+            console.error("Voice error:", event.error);
+            recognition.stop();
+        };
+
+    } else {
+        alert("Speech recognition not supported");
+    }
+};
+
